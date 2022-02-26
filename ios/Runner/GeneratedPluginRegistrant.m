669e0888632b9b9e726ce27433b29c77061b66d2
Shubham Bendal <97937230+shubhambendal@users.noreply.github.com>
2022-02-26 11:57:51 +0530

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<clevertap_plugin/CleverTapPlugin.h>)
+#import <clevertap_plugin/CleverTapPlugin.h>
+#else
+@import clevertap_plugin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CleverTapPlugin registerWithRegistrar:[registry registrarForPlugin:@"CleverTapPlugin"]];
+}
+
+@end
